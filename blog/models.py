c2c2dc9114609fd581969eaf227fe0f90ad3e47c
SahilMehdiyev <branchsahil@gmail.com>
2024-11-09 12:07:55 +0400

    rendering images faster in cloudflare

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.conf import settings
 from images.models import Image
 
+from images.services import get_image_url_from_cloudflare
+
+
 class BlogPost(models.Model):
-    image = models.ForeignKey(Image, null=True,blank=True,on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
-    content = models.TextField(blank=True,null=True)
+    content = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    
-    
-    
+    image = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def display_image_admin(self):
+        if not self.image:
+            return ""
+        cloudflare_id = self.image.cloudflare_id
+        return get_image_url_from_cloudflare(cloudflare_id, variant="admin")
